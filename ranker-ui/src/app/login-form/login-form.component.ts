fccf45223e41ea4693eca9705d435a74aaa8b3fb
nmarcin92 <nmarcin92@gmail.com>
2016-12-04 20:18:30 +0100

    materialize

---
+++ ranker-ui/src/app/login-form/login-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core"
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms"
+
+@Component({
+  selector: "login-form",
+  templateUrl: "./login-form.component.html"
+})
+
+export class LoginFormComponent {
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      "username": ["", Validators.required],
+      "password": ["", Validators.required]
+    });
+  }
+
+  onLogin() {
+    console.log(this.form);
+  }
+
+}
+
